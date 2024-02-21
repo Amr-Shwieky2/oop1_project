2019cc66396230e1101cc72403d7524af04a41ad
noor mhneha <nmhaneha@gmail.com>
2024-02-21 05:10:31 +0200

    sounds and information sectioonb

--- include/Screens.h
+++ include/Screens.h
@@ -2,6 +2,10 @@
 
 #include "Board.h"
 
+const int Sounds = 4;
+
+enum Sounds {OPEN , CLICK, TOMRUN, CATCH};
+
 class Screens
 {
 public:
@@ -9,24 +13,31 @@ public:
 
 	//void draw(sf::RenderWindow&) const;
 	void drawBackground(sf::RenderWindow&);
-	void drawStarterSection(sf::RenderWindow& );
-	void drawSoundButton(sf::RenderWindow& , bool);
-	void drawMap(sf::RenderWindow& window);
+	void drawStarterSection(sf::RenderWindow&);
+	void drawSoundButton(sf::RenderWindow&, bool);
+	void playPauseSound(int, bool);
 	void OpeningBackground(sf::RenderWindow&);
 
 
+
+
 private:
-	void drawStarter(sf::RenderWindow&) const;
+	//void drawStarter(sf::RenderWindow&) const;
+	void drawStarter(sf::RenderWindow& window, int spriteIndex) const;
 	sf::Text createText(const std::string&, const sf::Font&, unsigned int);
 	void drawTextInStarter(sf::Text&, sf::RenderWindow&, int);
 	//sf::Text createText(std::string, sf::Font, unsigned int, sf::RenderWindow&);
 
 	void setBackground();
+	void setCounters();
 	void setOpeningGame();
 	void setInformation();
-	void setSound();
+	void setSoundTexture();
+	void setSoundBuffers();
 
 	//Board m_board;
+	sf::Texture m_countersTexture[5];
+	sf::Sprite m_countersSprite[5];
 
 	sf::Texture m_backgroundTexture; // Texture for the background
 	sf::Sprite m_backgroundSprite; // Sprite for the background
@@ -38,5 +49,8 @@ private:
 
 	sf::Texture m_soundOn, m_soundOff;
 	sf::Sprite m_soundOnSprite, m_soundOffSprite;
+
+	sf::SoundBuffer m_soundBuffer[Sounds];
+	sf::Sound m_sound[Sounds];
 };
 
